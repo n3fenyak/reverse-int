2898b9527bbd1896e0ee3af4442a8472ebadc9b1
thenefenyak <nefenyak92@yandex.ru>
2024-08-29 13:46:16 +0300

    add index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
   
+    let str = n.toString();
+    
+    let reverseStr = str.split('').reverse().join('');
+    
+    // Convert the reversed string back to a number
+    return parseInt(reverseStr, 10);
 }
+
